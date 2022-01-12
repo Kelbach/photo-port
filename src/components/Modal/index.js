27f91312523c49cd900d2f3beb5ecd274e773923
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-12 01:34:41 -0500

    20.5.5 Modals work but now images wont render out of modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal() {
+function Modal({onClose, currentPhoto}) {
     const {name, category, description, index} = currentPhoto;
 
     return (
@@ -9,8 +9,8 @@ function Modal() {
         <h3 className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt="current category" />
         <p>{description}</p>
-        <button type="button" onClick={onClose}>
-            Close this modal
+        <button onClick={onClose} type="button">
+          Close this modal
         </button>
         </div>
       </div>
